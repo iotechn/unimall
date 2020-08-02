e3d107b5e8e29699a6d453a258789eaa7146406d
rize <cz@iotechn.com>
2020-08-02 17:02:18 +0800

    [conf] 关闭雪花算法

--- unimall-data/src/main/java/com/iotechn/unimall/data/config/db/MybatisPlusConfig.java
+++ unimall-data/src/main/java/com/iotechn/unimall/data/config/db/MybatisPlusConfig.java
@@ -4,32 +4,17 @@ import com.alibaba.druid.filter.Filter;
 import com.alibaba.druid.filter.stat.StatFilter;
 import com.alibaba.druid.pool.DruidDataSource;
 import com.baomidou.mybatisplus.autoconfigure.MybatisPlusProperties;
-import com.baomidou.mybatisplus.core.MybatisConfiguration;
-import com.baomidou.mybatisplus.core.MybatisXMLLanguageDriver;
-import com.baomidou.mybatisplus.core.config.GlobalConfig;
-import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
-import com.baomidou.mybatisplus.extension.spring.MybatisSqlSessionFactoryBean;
-import org.apache.ibatis.plugin.Interceptor;
-import org.apache.ibatis.session.SqlSessionFactory;
-import org.apache.ibatis.type.JdbcType;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import javax.sql.DataSource;
 import java.util.ArrayList;
 import java.util.List;
 
 @Configuration
 @EnableConfigurationProperties(value = {MybatisPlusProperties.class})
 public class MybatisPlusConfig {
-
-    private MybatisPlusProperties mybatisPlusProperties;
-
-    public MybatisPlusConfig(MybatisPlusProperties mybatisPlusProperties) {
-        this.mybatisPlusProperties = mybatisPlusProperties;
-    }
 
     /**
      * 数据源
@@ -55,37 +40,6 @@ public class MybatisPlusConfig {
         statFilter.setLogSlowSql(true);
         statFilter.setMergeSql(true);
         return statFilter;
-    }
-
-    /**
-     * Druid 监控
-     */
-//    @Bean
-//    public ServletRegistrationBean servletRegistrationBean(){
-//        return new ServletRegistrationBean(new StatViewServlet(),"/druid/*");
-//    }
-
-    @Bean("mybatisSqlSession")
-    public SqlSessionFactory sqlSessionFactory(DataSource dataSource, GlobalConfig globalConfig) throws Exception {
-        MybatisSqlSessionFactoryBean sqlSessionFactory = new MybatisSqlSessionFactoryBean();
-        sqlSessionFactory.setDataSource(dataSource);
-        sqlSessionFactory.setTypeAliasesPackage(mybatisPlusProperties.getTypeAliasesPackage());
-        sqlSessionFactory.setTypeEnumsPackage(mybatisPlusProperties.getTypeEnumsPackage());
-        MybatisConfiguration configuration = new MybatisConfiguration();
-        configuration.setDefaultScriptingLanguage(MybatisXMLLanguageDriver.class);
-        configuration.setJdbcTypeForNull(JdbcType.NULL);
-        sqlSessionFactory.setConfiguration(configuration);
-        sqlSessionFactory.setPlugins(new Interceptor[]{
-                new PaginationInterceptor(),
-        });
-        sqlSessionFactory.setGlobalConfig(globalConfig);
-        return sqlSessionFactory.getObject();
-    }
-
-    @Bean
-    public GlobalConfig globalConfiguration() {
-        GlobalConfig conf = new GlobalConfig();
-        return conf;
     }
 
 }
