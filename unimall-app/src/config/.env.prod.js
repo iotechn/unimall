5abe9e35d81c16dd83d359a151232554af9288f6
william.wei <william.wei@yimlink.com>
2024-06-19 14:28:49 +0800

    [conf] v4 url

--- unimall-app/src/config/.env.prod.js
+++ unimall-app/src/config/.env.prod.js
@@ -1,5 +1,5 @@
 const config = {
-  baseUrl: 'https://unimall.v3.dobbinsoft.com',
+  baseUrl: 'https://unimallv4.dobbinsoft.com/unimall',
   h5Appid: 'wxb66b599f7f61b46f',
   debug: false,
   OSS_PROVIDER: 'qcloud'
