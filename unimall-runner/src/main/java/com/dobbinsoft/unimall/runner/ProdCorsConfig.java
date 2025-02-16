77f6d1aa0245581d942935aedbfccb49c9d1df55
william.wei <william.wei@yimlink.com>
2025-02-16 19:57:00 +0800

    [dev] prod 开启跨域

---
+++ unimall-runner/src/main/java/com/dobbinsoft/unimall/runner/ProdCorsConfig.java
@@ -0,0 +1,28 @@
+package com.dobbinsoft.unimall.runner;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+@Profile(value = {"prod", "deliver"})
+public class ProdCorsConfig {
+
+    private CorsConfiguration buildConfig() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+        return corsConfiguration;
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", buildConfig());
+        return new CorsFilter(source);
+    }
+}
